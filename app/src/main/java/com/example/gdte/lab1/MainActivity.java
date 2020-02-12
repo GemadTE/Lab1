0f8c68eb1db036b6a4946ce92fd933b19415abd4
Gema del Toro <gema.del102@alu.ulpgc.es>
2020-02-12 11:05:15 +0100

    Inicial Documentation

--- app/src/main/java/com/example/gdte/lab1/MainActivity.java
+++ app/src/main/java/com/example/gdte/lab1/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Documentacion inicial
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
